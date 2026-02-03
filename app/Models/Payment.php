42da8132a91d0e32d380a58fd879f76cf7c24c23
fhmcoding <moutcho70@gmail.com>
2026-02-03 02:38:15 +0100

    ='fix'

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\OptionalPagination;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Payment extends Model
 {
