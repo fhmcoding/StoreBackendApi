ab47b9e44d492d5130479c31224e90516455de32
fhmcoding <moutcho70@gmail.com>
2026-03-13 08:09:28 +0000

    =''

--- app/Http/Controllers/V1/Backoffice/Products/IndexController.php
+++ app/Http/Controllers/V1/Backoffice/Products/IndexController.php
@@ -7,18 +7,20 @@ use App\Http\Resources\Backoffice\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\JsonResponse;
 use Spatie\QueryBuilder\QueryBuilder;
+use Illuminate\Http\Request;
 
 
 class IndexController extends Controller
 {
-    public function __invoke():JsonResponse
+    public function __invoke(Request $request):JsonResponse
     {
         return $this->success(
             ProductResource::collection(
                 QueryBuilder::for(Product::class)
                     ->with('images','category','brand')
-                    ->allowedFilters('name','brand_id','product_code')
+                    ->allowedFilters('name','product_code')
                     ->allowedIncludes('category','brand')
+                    ->where('brand_id',$request->filter['brand_id'] ?? null)
                     ->latest()
                     ->optionalPagination()
             )->response()->getData(true)
