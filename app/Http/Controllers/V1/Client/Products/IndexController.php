f66932694c920a9c1a2dd870ce54741726c1c27b
fhmcoding <moutcho70@gmail.com>
2026-04-12 21:33:30 +0100

    ='fix'

--- app/Http/Controllers/V1/Client/Products/IndexController.php
+++ app/Http/Controllers/V1/Client/Products/IndexController.php
@@ -40,7 +40,6 @@ class IndexController extends Controller
                 'product_code' => $product->product_code,
                 'description' => $product->description,
                 'image_url' => count($product->images) > 0 ? Storage::disk('public')->url($product->images[0]->image_url) : null,
-                'size' => $product->size,
                 'sale_price' => $product->price,
                 'category' => $product->category,
                 'brand' => $product->brand
