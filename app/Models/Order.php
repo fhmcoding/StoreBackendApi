44a183c4a23286002684423f538fdb2248f3716f
fhmcoding <moutcho70@gmail.com>
2026-04-12 18:42:23 +0100

    ='fix'

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -36,13 +36,13 @@ class Order extends Model
             $model->order_ref = $model->generateOrdeReference();
         });
 
-        //if(Auth::check()){
-            if(!Auth::guard('user')->user()->hasPermissionTo('all-orders')){
-                static::addGlobalScope('user', static function (Builder $builder): void {
-                    $builder->where('caissier_id', Auth::user()->id);
-                });
-           }
-        //}
+        // if(Auth::check()){
+        //     if(!Auth::guard('user')->user()->hasPermissionTo('all-orders')){
+        //         static::addGlobalScope('user', static function (Builder $builder): void {
+        //             $builder->where('caissier_id', Auth::user()->id);
+        //         });
+        //     }
+        // }
     }
 
     public function customer():BelongsTo
