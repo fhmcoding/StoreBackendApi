54a2d377b7f998dbae0e8ef47ab1cf56de987a12
fhmcoding <moutcho70@gmail.com>
2026-04-12 21:59:49 +0100

    ='product_code'

--- app/Http/Controllers/V1/Client/Products/ShowController.php
+++ app/Http/Controllers/V1/Client/Products/ShowController.php
@@ -4,22 +4,47 @@ namespace App\Http\Controllers\V1\Client\Products;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Client\ProductResource;
-use App\Models\ProductGroup;
+use App\Models\Product;
 use Illuminate\Http\JsonResponse;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class ShowController extends Controller
 {
 
-    public function __invoke(ProductGroup $product):JsonResponse
+    public function __invoke(Product $product):JsonResponse
     {
+
+        $product = Product::findOrFail($id);
+
+        preg_match('/(\d+\s?ml)$/i', $product->name, $matches);
+
+        $baseName = trim(preg_replace('/(\s*\d+\s?ml)$/i', '', $product->name));
+
+        $similarProducts = Product::where('name', 'LIKE', $baseName . '%')->get();
+
+        $result = [
+            'name' => $baseName,
+            'products' => $similarProducts->map(function ($item) {
+
+                preg_match('/(\d+\s?ml)$/i', $item->name, $matches);
+                $size = isset($matches[1])
+                    ? strtoupper(str_replace(' ', '', $matches[1]))
+                    : null;
+
+                return [
+                    'id' => $item->id,
+                    'product_code' => $item->product_code,
+                    'size' => $size,
+                    'price' => $item->price,
+                ];
+            })->values()
+        ];
+
         return $this->success(
-            ProductResource::make(
-                QueryBuilder::for(ProductGroup::class)
-                    ->with('products','products.offers')
-                    ->allowedIncludes('brand','category','images')
-                    ->findOrFail($product->id)
-            )
+            [
+                'product' => $product,
+                'similarProducts'=>$result
+            ]
         );
     }
 }
