3b7c353e971ca19fb4172f3e3a12f70734abc576
fhmcoding <moutcho70@gmail.com>
2026-04-30 13:45:09 +0100

    ='fix'

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\OptionalPagination;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Auth;
+use Illuminate\Database\Eloquent\Builder;
 
 class Expense extends Model
 {
@@ -14,7 +16,19 @@ class Expense extends Model
 
     protected $guarded = ['id'];
 
+    public static function boot()
+    {
+        parent::boot();
 
+
+        if(Auth::check()){
+            if(!Auth::guard('user')->user()->hasPermissionTo('expense-all')){
+                static::addGlobalScope('user', static function (Builder $builder): void {
+                    $builder->where('user_id', Auth::user()->id);
+                });
+            }
+        }
+    }
      public function user():BelongsTo
     {
         return $this->belongsTo(User::class);
