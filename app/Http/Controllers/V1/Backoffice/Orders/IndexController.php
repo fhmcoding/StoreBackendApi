97a9df16d60c790f061f8958c7dde24a62343fa1
fhmcoding <moutcho70@gmail.com>
2026-07-17 12:00:19 +0100

    =fix

--- app/Http/Controllers/V1/Backoffice/Orders/IndexController.php
+++ app/Http/Controllers/V1/Backoffice/Orders/IndexController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\Backoffice\OrderResource;
 use Illuminate\Http\JsonResponse;
 use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
+
 use App\Models\Order;
 
 class IndexController extends Controller
@@ -17,7 +19,15 @@ class IndexController extends Controller
                 QueryBuilder::for(Order::class)
                     ->with('user','customer','statusHistory','user','caissier')
                     ->allowedIncludes('products','payments','customer','productsCount')
-                    ->allowedFilters('user_id','caissier_id','created_at')
+                    ->allowedFilters('user_id','caissier_id')
+                    ->allowedFilters([
+                        AllowedFilter::callback('created_from', fn ($query, $value) =>
+                            $query->whereDate('created_at', '>=', $value)
+                        ),
+                        AllowedFilter::callback('created_to', fn ($query, $value) =>
+                            $query->whereDate('created_at', '<=', $value)
+                        ),
+                    ])
                     //->where('caissier_id',auth()->user()->id)
                     ->latest()
                     ->optionalPagination()
