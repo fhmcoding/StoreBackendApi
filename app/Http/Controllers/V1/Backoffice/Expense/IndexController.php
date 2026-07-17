5b98edfba02e7755b65f77da8fd95c363c34d308
fhmcoding <moutcho70@gmail.com>
2026-07-17 17:48:17 +0100

    =expense

--- app/Http/Controllers/V1/Backoffice/Expense/IndexController.php
+++ app/Http/Controllers/V1/Backoffice/Expense/IndexController.php
@@ -8,6 +8,7 @@ use App\Models\Expense;
 use App\Http\Resources\Backoffice\ExpenseResource;
 use Illuminate\Http\JsonResponse;
 use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
 
 class IndexController extends Controller
 {
@@ -23,7 +24,14 @@ class IndexController extends Controller
             ExpenseResource::collection(
                 QueryBuilder::for(Expense::class)
                     ->allowedIncludes('user')
-                    ->allowedFilters('user_id','created_at')
+                    ->allowedFilters(['user_id','created_at',
+                    AllowedFilter::callback('created_from', fn ($query, $value) =>
+                            $query->whereDate('created_at', '>=', $value)
+                        ),
+                        AllowedFilter::callback('created_to', fn ($query, $value) =>
+                            $query->whereDate('created_at', '<=', $value)
+                        )
+                        ])
                     ->optionalPagination()
             )->response()->getData(true)
         );
--- app/Http/Controllers/V1/Backoffice/Orders/IndexController.php
+++ app/Http/Controllers/V1/Backoffice/Orders/IndexController.php
@@ -19,7 +19,7 @@ class IndexController extends Controller
                 QueryBuilder::for(Order::class)
                     ->with('user','customer','statusHistory','user','caissier')
                     ->allowedIncludes('products','payments','customer','productsCount')
-                   ->allowedFilters(['user_id','caissier_id',
+                   ->allowedFilters(['user_id','caissier_id','created_at'
                         AllowedFilter::callback('created_from', fn ($query, $value) =>
                             $query->whereDate('created_at', '>=', $value)
                         ),
